181ae571fa85e8c31e11570f0fea1626d11ece8f
Gwynbleidd <vanimendes344@gmail.com>
2025-04-15 16:40:12 +0300

    [FEAT] - Responsive landing page for phone

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -4,20 +4,42 @@ const button = document.getElementById("start-button")
 const left = document.getElementById("start-up")
 const right = document.getElementById("start-up-images")
 
+const phone = window.matchMedia('(max-width:450px)')
+
 
 button.addEventListener("click", () => {
 
-    left.style.transform = "translateX(50vw)"
-    right.style.transform = "translateX(-50vw)"
+    if (phone.matches) {
 
-    setTimeout(() => {
-        
-        window.location.href = "assets/pages/test.html"
-    }, 1450);
-    setTimeout(() => {
-        
-        main.classList.add("disapear")
-    }, 500);
+        right.classList.add("appear")
+        setTimeout(() => {
+            right.style.opacity = "100%"
+        },1000)
 
+        left.classList.add("disapear")
+        setTimeout(() => {
+            left.style.opacity = "0%"
+        },1000)
 
+        setTimeout(() => {  
+            main.classList.add("disapear")
+        }, 3500);
+
+        setTimeout(() => {
+            window.location.href = "assets/pages/test.html"
+        }, 4000);
+
+    }
+    else {
+        left.style.transform = "translateX(50vw)"
+        right.style.transform = "translateX(-50vw)"
+
+        setTimeout(() => {  
+            main.classList.add("disapear")
+        }, 500);
+
+        setTimeout(() => {
+            window.location.href = "assets/pages/test.html"
+        }, 1450);
+    }
 })
