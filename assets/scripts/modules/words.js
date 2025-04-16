8cd143bce20068ae3e5942666dce68ac4997da93
Gwynbleidd <vanimendes344@gmail.com>
2025-04-16 09:48:01 +0300

    [UPD] - upgraded wordlist

--- assets/scripts/modules/words.js
+++ assets/scripts/modules/words.js
@@ -1,23 +1,71 @@
 export const wordsList = {
-    easy: [
-        'cat', 'dog', 'sun', 'run', 'go', 'big', 'red', 'blue', 'yes', 'no',
-        'up', 'down', 'in', 'out', 'on', 'off', 'hot', 'cold', 'fast', 'slow',
-        'happy', 'sad', 'jump', 'walk', 'eat', 'drink', 'car', 'ball', 'hat', 'bed',
-        'mom', 'dad', 'man', 'girl', 'boy', 'book', 'toy', 'fun', 'play', 'stop',
-        'start', 'look', 'see', 'help', 'name', 'fish', 'bird', 'cake', 'tree', 'home'
-    ],
-    medium: [
-        'about', 'above', 'across', 'action', 'afraid', 'alive', 'alone', 'angry', 'animal', 'answer',
-        'apple', 'arrive', 'aunt', 'baby', 'back', 'balloon', 'banana', 'basket', 'begin', 'believe',
-        'beside', 'better', 'between', 'birthday', 'black', 'bottle', 'bottom', 'breathe', 'brother', 'build',
-        'butter', 'button', 'carry', 'center', 'chicken', 'circle', 'clean', 'climb', 'close', 'cloud',
-        'color', 'corner', 'country', 'cousin', 'crayon', 'dance', 'danger', 'decide', 'delight', 'dollar'
-    ],
-    hard: [
-        'abundance', 'accomplishment', 'acknowledge', 'advantageous', 'allegiance', 'alternative', 'appreciation', 'architecture', 'articulate', 'assessment',
-        'circumference', 'collaboration', 'commencement', 'comparison', 'complication', 'comprehensive', 'concentration', 'conclusion', 'configuration', 'consequence',
-        'consideration', 'constellation', 'contribution', 'controversial', 'cooperation', 'correspondence', 'declaration', 'dedication', 'deliberation', 'demonstration',
-        'determination', 'disadvantage', 'discovery', 'effectiveness', 'elaboration', 'encouragement', 'environment', 'establishment', 'evaluation', 'exaggeration',
-        'experience', 'explanation', 'extraordinary', 'imagination', 'improvement', 'independence', 'information', 'instrument', 'interpretation', 'investigation'
-    ]
+    english : {
+        easy: [
+            'cat', 'dog', 'sun', 'run', 'go', 'big', 'red', 'blue', 'yes', 'no',
+            'up', 'down', 'in', 'out', 'on', 'off', 'hot', 'cold', 'fast', 'slow',
+            'happy', 'sad', 'jump', 'walk', 'eat', 'drink', 'car', 'ball', 'hat', 'bed',
+            'mom', 'dad', 'man', 'girl', 'boy', 'book', 'toy', 'fun', 'play', 'stop',
+            'start', 'look', 'see', 'help', 'name', 'fish', 'bird', 'cake', 'tree', 'home'
+        ],
+        medium: [
+            'about', 'above', 'across', 'action', 'afraid', 'alive', 'alone', 'angry', 'animal', 'answer',
+            'apple', 'arrive', 'aunt', 'baby', 'back', 'balloon', 'banana', 'basket', 'begin', 'believe',
+            'beside', 'better', 'between', 'birthday', 'black', 'bottle', 'bottom', 'breathe', 'brother', 'build',
+            'butter', 'button', 'carry', 'center', 'chicken', 'circle', 'clean', 'climb', 'close', 'cloud',
+            'color', 'corner', 'country', 'cousin', 'crayon', 'dance', 'danger', 'decide', 'delight', 'dollar'
+        ],
+        hard: [
+            'abundance', 'accomplishment', 'acknowledge', 'advantageous', 'allegiance', 'alternative', 'appreciation', 'architecture', 'articulate', 'assessment',
+            'circumference', 'collaboration', 'commencement', 'comparison', 'complication', 'comprehensive', 'concentration', 'conclusion', 'configuration', 'consequence',
+            'consideration', 'constellation', 'contribution', 'controversial', 'cooperation', 'correspondence', 'declaration', 'dedication', 'deliberation', 'demonstration',
+            'determination', 'disadvantage', 'discovery', 'effectiveness', 'elaboration', 'encouragement', 'environment', 'establishment', 'evaluation', 'exaggeration',
+            'experience', 'explanation', 'extraordinary', 'imagination', 'improvement', 'independence', 'information', 'instrument', 'interpretation', 'investigation'
+        ]
+    },
+    french : {
+        easy: [
+            'chat', 'pain', 'feu', 'main', 'lait', 'eau', 'nez', 'clé', 'ami', 'rue',
+            'lune', 'jour', 'vent', 'neige', 'bleu', 'chaud', 'froid', 'vite', 'lent', 'beau',
+            'rire', 'pleur', 'voir', 'dire', 'faire', 'aimer', 'jouer', 'tomber', 'manger', 'boire',
+            'maison', 'porte', 'lit', 'papa', 'maman', 'bébé', 'livre', 'chien', 'oiseau', 'poule',
+            'chaton', 'soleil', 'sac', 'voiture', 'clé', 'table', 'arbre', 'pain', 'fromage', 'chapeau'
+        ],
+        medium: [
+            'fenêtre', 'matin', 'chanson', 'forêt', 'sourire', 'sauter', 'dessiner', 'partager', 'ouvrir', 'fermer',
+            'bouteille', 'paysage', 'camion', 'valise', 'travail', 'question', 'réponse', 'habitude', 'lecture', 'écrire',
+            'silence', 'parfum', 'cascade', 'cheval', 'grenouille', 'poisson', 'montagne', 'cloche', 'chaise', 'hiver',
+            'printemps', 'vacances', 'cheveux', 'cousin', 'famille', 'histoire', 'attention', 'courage', 'vêtement', 'lampe',
+            'musique', 'couleur', 'étoile', 'nuage', 'chemin', 'vélo', 'garage', 'ami', 'récréation', 'gâteau'
+        ],
+        hard: [
+            'apprentissage', 'bienveillance', 'responsabilité', 'persévérance', 'générosité', 'indépendance', 'tolérance', 'imagination', 'organisation', 'développement',
+            'compréhension', 'réflexion', 'créativité', 'expression', 'communication', 'inspiration', 'ambition', 'confiance', 'motivation', 'exploration',
+            'éducation', 'observation', 'concentration', 'collaboration', 'méthodologie', 'expérimentation', 'anticipation', 'interprétation', 'curiosité', 'émerveillement',
+            'respectueux', 'équilibre', 'émotionnel', 'adaptabilité', 'écoute', 'coopération', 'gestion', 'stratégie', 'intelligence', 'culture',
+            'réussite', 'harmonie', 'relationnel', 'création', 'innovation', 'authenticité', 'complexité', 'analyse', 'cohérence', 'valeur'
+        ]        
+    },
+    malagasy : {
+        easy: [
+            'akoho', 'alika', 'maso', 'rano', 'vary', 'voa', 'manja', 'mafana', 'mangatsiaka', 'eny',
+            'tsia', 'ambony', 'ambany', 'mivoaka', 'miditra', 'mahita', 'mihomehy', 'mitomany', 'mandeha', 'mijanona',
+            'matory', 'mifoha', 'ray', 'reny', 'zaza', 'trano', 'lamba', 'sakafo', 'kitapo', 'penina',
+            'boky', 'soavaly', 'tsofa', 'kabone', 'fitaratra', 'afo', 'tendrombohitra', 'hazo', 'voankazo', 'fahazavana',
+            'kintana', 'lanitra', 'mamy', 'masira', 'menaka', 'vary', 'lasopy', 'akoho', 'mofo', 'voanio'
+        ],
+        medium: [
+            'fianarana', 'fahasalamana', 'fitiavana', 'fiainana', 'fanampiana', 'fiangonana', 'fahazotoana', 'tsiky', 'alahelo', 'hasambarana',
+            'fampianarana', 'kilasy', 'mpampianatra', 'mpianatra', 'fahendrena', 'fialamboly', 'zavamaniry', 'biby', 'voasary', 'mangahazo',
+            'masomboly', 'vokatry', 'tany', 'lalana', 'fahasambarana', 'fiarovana', 'faharetana', 'tombontsoa', 'fanajana', 'fampahatsiahivana',
+            'fanombohana', 'fampandrosoana', 'manampy', 'mamaky', 'manoratra', 'mivavaka', 'manadio', 'mampianatra', 'manasa', 'mitsiky',
+            'maniry', 'manome', 'mampihomehy', 'mampalahelo', 'mamelona', 'mampitony', 'manazava', 'mitsangana', 'miala', 'miverina'
+        ],
+        hard: [
+            'fampandrosoana', 'fampitomboana', 'fifandraisana', 'fahaleovantena', 'fandaminana', 'fampaherezana', 'fanapahan-kevitra', 'fampivelarana', 'fampianarana', 'ambony', 'fanatsarana',
+            'fahalehibeazana', 'fiaviana', 'fandaharana', 'fampanantenana', 'fandaminana', 'ara-tsaina', 'fandinihana', 'fampitahana', 'fahalalana', 'fampanofanana', 'fahaiza-manao',
+            'fandaminana','sosialy', 'fampanajana', 'fanombohana', 'stratejika', 'fanombanana', 'fampiharana', 'fampivoarana', 'fanentanana', 'fanaovan-tsonia', 'fanapariahana', 'fampifandanjana',
+            'fanovàna', 'fampifandraisana', 'famaranana', 'fanantenana', 'fahatsiarovana', 'fandresena', 'fampihavanana', 'fanavaozana', 'fanompoam-pivavahana', 'fampielezana',
+            'fanadihadiana', 'fanatontoloana', 'fanekena', 'fanovàna','ara-kolontsaina', 'fanombohana','manara-penitra', 'fahazotoana','ambony', 'fampisehoana', 'fampiasana', 'fanantenana', 'lavitra', 'fahavononana'
+        ]        
+    }
 };
