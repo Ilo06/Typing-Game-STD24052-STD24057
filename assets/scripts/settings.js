2d5142b1c0c47be14fabdf714e837e470b8dfb30
Gwynbleidd <vanimendes344@gmail.com>
2025-04-19 21:00:10 +0300

    [FIX] settings menu fixed

--- assets/scripts/settings.js
+++ assets/scripts/settings.js
@@ -37,28 +37,19 @@ let currentTheme = {
     tertyColor : '#1F2937'
 }
 
-if (previousTheme.selectedTheme == "default" || previousTheme.selectedTheme == "wood" ) {
-    root.style.setProperty("--secondary-color", previousTheme.secondaryColor)
-    root.style.setProperty("--primary-color", previousTheme.primaryColor)
-    root.style.setProperty("--terty-color", previousTheme.tertyColor)
-} else {
-    root.style.setProperty("--secondary-color", previousTheme.primaryColor)
-    root.style.setProperty("--primary-color", previousTheme.secondaryColor)
-    root.style.setProperty("--terty-color", previousTheme.tertyColor)
-}
 
 desertSand.addEventListener("click", () => {
     root.style.setProperty("--terty-color", "#6E6E6E ")
     root.style.setProperty("--primary-color", "#838181")
     root.style.setProperty("--secondary-color", "#2A4551")
-
+    
     currentTheme.secondaryColor = "#838181";
     currentTheme.primaryColor = "#2A4551";
     currentTheme.tertyColor = "#6E6E6E";
     currentTheme.selectedTheme = "desertSand";
-
+    
     localStorage.setItem("theme", JSON.stringify(currentTheme));
-
+    
     customBox.classList.add("hide")
 })
 
@@ -66,7 +57,7 @@ wood.addEventListener("click", () => {
     root.style.setProperty("--secondary-color", "#BFA88F")
     root.style.setProperty("--primary-color", "#4B3B30")
     root.style.setProperty("--terty-color", "#2C1E16")
-
+    
     currentTheme.primaryColor = "#4B3B30";
     currentTheme.secondaryColor = "#BFA88F";
     currentTheme.tertyColor = "#2C1E16";
@@ -87,21 +78,21 @@ steelGray.addEventListener("click", () => {
     currentTheme.selectedTheme = "steelGray";
     
     localStorage.setItem("theme", JSON.stringify(currentTheme));
-
+    
     customBox.classList.add("hide")
 })
 defaultColor.addEventListener("click", () => {
     root.style.setProperty("--secondary-color", "#C6C8CC")
     root.style.setProperty("--primary-color", "#374151")
     root.style.setProperty("--terty-color", "#1F2937")
-
+    
     currentTheme.primaryColor = "#374151";
     currentTheme.secondaryColor = "#C6C8CC";
     currentTheme.tertyColor = "#1F2937";
     currentTheme.selectedTheme = "default";
-
+    
     localStorage.setItem("theme", JSON.stringify(currentTheme));
-
+    
     customBox.classList.add("hide")
 })
 
@@ -115,11 +106,11 @@ closeIcon.addEventListener("click", () => {
 applyButton.addEventListener("click", () => {
     root.style.setProperty("--secondary-color", customSecondaryColor.value)
     root.style.setProperty("--primary-color", customPrimaryColor.value)
-
+    
     currentTheme.primaryColor = customSecondaryColor.value;
     currentTheme.secondaryColor = customPrimaryColor.value;
     currentTheme.selectedTheme = "custom";
-
+    
     localStorage.setItem("theme", JSON.stringify(currentTheme));
 })
 
@@ -128,7 +119,7 @@ settingsCloseIcon.addEventListener("click", () => {
 })
 
 settingsButton.addEventListener("click", () => {
-
+    
     settings.classList.remove("hide")
 })
 
@@ -165,3 +156,13 @@ comic.addEventListener("click", () => {
     displayedWord.style.fontFamily = "'Comic sans ms'"
     settings.classList.add("hide")
 })
+
+if (previousTheme.selectedTheme == "default" || previousTheme.selectedTheme == "wood" ) {
+    root.style.setProperty("--secondary-color", previousTheme.secondaryColor)
+    root.style.setProperty("--primary-color", previousTheme.primaryColor)
+    root.style.setProperty("--terty-color", previousTheme.tertyColor)
+} else {
+    root.style.setProperty("--secondary-color", previousTheme.primaryColor)
+    root.style.setProperty("--primary-color", previousTheme.secondaryColor)
+    root.style.setProperty("--terty-color", previousTheme.tertyColor)
+}
